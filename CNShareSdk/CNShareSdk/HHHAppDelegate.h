85602829e04e57fb6535155ee0ffe9a8bda099f4
woaiaudi <admin@admindeiMac.local>
2014-06-06 15:41:39 +0800

    init  new

---
+++ CNShareSdk/CNShareSdk/HHHAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  HHHAppDelegate.h
+//  CNShareSdk
+//
+//  Created by admin on 14-6-6.
+//  Copyright (c) 2014年 冲浪科技. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HHHAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ CNShareSdk/CNShareSdk/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  CNShareSdk
+//
+//  Created by admin on 14-6-6.
+//  Copyright (c) 2014年 冲浪科技. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "HHHAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([HHHAppDelegate class]));
+    }
+}
